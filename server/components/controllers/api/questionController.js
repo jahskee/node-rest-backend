25cf28bccce80ec3c2f0b0452f2fa96d633031ea
Jazon Lubaton <jahskee@yahoo.com>
2018-05-09 15:33:31 -0400

    Very Stable - custom search  is working

--- server/components/controllers/api/questionController.js
+++ server/components/controllers/api/questionController.js
@@ -1,12 +1,27 @@
 //const DataController = require('./crud/_dataController');
-const Question = require('../../models/questionModel');
-const DataController = require('./crud/_dataController');
+const Question = require("../../models/questionModel");
+const DataController = require("./crud/_dataController");
 
 class QuestionController extends DataController {
-    constructor(model) {
-        super(model)
-    }
+  constructor(model) {
+    super(model);
 
+    this.searchScienceIds = async (req, res) => {
+      let collection;
+
+      try {
+        collection = await this._search({'category': /Science/}, ['_id'], 10);
+        res.json(collection);
+      } catch (err) {
+        console.log(err);
+        res.end();
+      }
+    };
+  }
+
+  searchScienceIds() {
+      
+  }
 }
 
 const controller = new QuestionController(Question);
--- server/components/routes/api/api-questions.js
+++ server/components/routes/api/api-questions.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 const questionController = require('../../controllers/api/questionController')
 
+
 router.get('/', questionController.list);
 router.post('/create', questionController.create);
 
@@ -11,5 +12,8 @@ router.post('/create', questionController.create);
 //router.get('/:id', questionController.read);
 //router.put('/update', questionController.update);
 //router.delete('/delete/:id', questionController.delete);
+
+// custom searches
+router.get('/science/ids', questionController.searchScienceIds);
 
 module.exports = router;

f864e8f25ebb116e86fc793f6ea33f106cf08936
jahskee <jahskee@yahoo.com>
2018-06-29 00:59:36 -0400

    update

--- server/app.js
+++ server/app.js
@@ -100,7 +100,7 @@ app.use("/api/books", apiBooks);
 
 
 // add angular files
-app.use("/", express.static(path.join(__dirname, "../../weather/build")));
+//app.use("/", express.static(path.join(__dirname, "../../weather/build")));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
